dc6d9bae379ce65a08710717c6ccaf3b319fdafd
jcaw <toastedjcaw@gmail.com>
2026-05-13 15:07:46 +0100

    Four Around Four first draft

--- 2026_summer/four_around_four.js
+++ 2026_summer/four_around_four.js
@@ -1,72 +1,60 @@
 // Four Around Four
 let part = 1
-
-// UNCOMMENT PART 2 THEN CLICK "update" TO SWITCH TO PART 2
-// part = 2
+part = 2
 
 
 /////////////////////////////////////////////////////
 // CODE BELOW - DO NOT TOUCH
 
-setCpm(95/4)
+setCpm(100/4)
 
-let kick = s("[bd ~ bd ~]*4").bank("RolandTR909")
-  .lpf(240)
-  .lpenv(2.4)
-  .decay(0.68)
-  .room(0.12)
-  .clip(1.15)
-  .distort(0.14)
-  .gain(0.92)
+
+// TODO: Fill out the bass with a second layer - arpeggios
+
+
+// TODO: Nicer kick sound. Thicker, more techno
+let kick = s("[bd ~]*4").bank("RolandTR909")
+  .lpf(100)
+  .lpenv(1.2)
+  .room(0.1)
+  .clip(1)
 
 let full_notes
 if (part == 1) {
   // Galopede
   full_notes = cat(
-    "[g1 d1] e1 a1 d1",
-    "[g1 d1] e1 [a1 d1] g1",
-    "[g1 c1] [g1 d1] [g1 c1] d1",
-    "[g1 c1] [g1 d1] [c1 d1] g1",
-    "g1 g1 [g1 c1] d1",
-    "g1 g1 [g1 d1] g1",
+    "g1 g1 g1 g1",
+    "d1 d1 d1 d1",
+    "g1 g1 g1 g1",
+    "d1 d1 d1 d1",
+    "g1 g1 g1 g1",
+    "d1 d1 d1 d1",
   ).note()
 } else {
   // Three Around Three
   full_notes = cat(
-    "[g1 d1] [g1 c1] c1 [c1 d1]",
-    "[g1 d1] [g1 c1] d1 g1",
-    "g1 c1 g1 d1",
-    "g1 c1 [c1 d1] g1",
+    "g1 g1 d1 d1",
+    "g1 g1 d1 d1",
+    "g1 g1 d1 d1",
+    "g1 g1 d1 d1",
   ).note()
 }
 
-let bassline = full_notes.s("square").decay(0.18)
-  .struct("[~ ~ ~ x ~ ~ x ~]*4").slow(1)
-  .sustain(0)
-  .clip(1.15)
-  .distort(0.18)
-  .lpf(slider(430, 180, 2400))
-  .lpq(7)
-  .lpenv(slider(4.4, 1.2, 7))
-  .delay(0.1)
-  .gain(0.54)
-
-let bass_arp = full_notes.s("triangle").decay(0.12)
-  .struct("[x ~ x ~ ~ x ~ x]*4").slow(1)
-  .sustain(0)
-  .clip(0.75)
-  .lpf(sine.range(520, 1600).slow(12))
-  .lpq(5)
-  .lpenv(3.2)
-  .delay(0.16)
-  .room(0.3)
-  .gain(0.22)
+// TODO: More like a laser sound?
+let bassline = full_notes.s("supersaw").decay(0.3)
+  .struct("[~ ~ ~ x ~ ~ x ~]*2").slow(1)
+  //.clip(sine.range(0.8, 1.0).slow(8))
+  .clip(2)
+  .lpf(slider(335, 200, 2000))
+  .lpenv(slider(2.0832, 1.2, 6))
+  .gain(0.8)
+  // .distort(0.3)
 
 let count_in = s("[hh]*4").bank("RolandTR909").lpf(1800).lpenv(0.5).decay(3).room(0.01).gain(0.3)
 
-offset = 4 - 1/2
+offset = 6 - 1/2
 $: arrange(
-  [offset,     stack(kick, bassline, bass_arp)],
-  [1/2,        stack(kick, bassline.late(offset), bass_arp.late(offset), count_in.slow(1/2))],
-  [4294967296, stack(kick, bassline, bass_arp)],
+  [offset,     stack(kick, bassline)],
+  [1/2,        stack(kick, bassline.late(offset), count_in.slow(1/2))],
+  [4294967296, stack(kick, bassline)],
 )
