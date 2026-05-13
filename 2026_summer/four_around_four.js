e1ebca4a9a3e72297a5b777d6066107c9790a2c6
jcaw <toastedjcaw@gmail.com>
2026-05-13 15:22:17 +0100

    Refactor kick and bass sounds for improved techno vibe

--- 2026_summer/four_around_four.js
+++ 2026_summer/four_around_four.js
@@ -1,6 +1,6 @@
 // Four Around Four
 let part = 1
-part = 2
+// part = 2
 
 
 /////////////////////////////////////////////////////
@@ -12,12 +12,28 @@ setCpm(100/4)
 // TODO: Fill out the bass with a second layer - arpeggios
 
 
-// TODO: Nicer kick sound. Thicker, more techno
 let kick = s("[bd ~]*4").bank("RolandTR909")
-  .lpf(100)
-  .lpenv(1.2)
-  .room(0.1)
-  .clip(1)
+  .lpf(260)
+  .lpenv(2.4)
+  .decay(0.68)
+  .room(0.12)
+  .clip(1.16)
+  .distort(0.15)
+  .gain(0.92)
+
+// FIXME: Snare sounds shit
+// let reverse_snare = s("[~ sd]*4").bank("RolandTR909")
+//   .rev()
+//   .decay(0.42)
+//   .room(0.38)
+//   .gain(0.24)
+
+// TODO: More hiss
+let hihat = s("hh").bank("RolandTR909")
+  .struct("[~ ~ x x ~ ~ x ~]*2")
+  .decay(0.08)
+  .hpf(5200)
+  .gain(0.24)
 
 let full_notes
 if (part == 1) {
@@ -40,21 +56,42 @@ if (part == 1) {
   ).note()
 }
 
-// TODO: More like a laser sound?
-let bassline = full_notes.s("supersaw").decay(0.3)
+let bass_sub = full_notes.s("sine").decay(0.55)
+  .struct("[~ x]*4").slow(1)
+  .sustain(0)
+  .clip(1.35)
+  .distort(0.08)
+  .lpf(120)
+  .gain(0.58)
+
+// TODO: This effect is interesting, let's duplicate it with a slider to fade it in when useful, but it's not the right sound. I want a more typical techno sound, something from outside this repo.
+let bass_mid = full_notes.s("square").decay(0.22)
   .struct("[~ ~ ~ x ~ ~ x ~]*2").slow(1)
-  //.clip(sine.range(0.8, 1.0).slow(8))
-  .clip(2)
-  .lpf(slider(335, 200, 2000))
-  .lpenv(slider(2.0832, 1.2, 6))
-  .gain(0.8)
-  // .distort(0.3)
+  .sustain(0)
+  .clip(1.2)
+  .distort(0.18)
+  .lpf(slider(360, 120, 1800))
+  .lpq(6)
+  .lpenv(slider(2.6, 0.8, 5))
+  .gain(0.38)
+
+// let bass_mid = full_notes.s("square").decay(0.22)
+//   .struct("[x x x x x x]*2").slow(1)
+//   .sustain(0)
+//   .clip(1.2)
+//   .distort(0.18)
+//   .lpf(slider(360, 120, 1800))
+//   .lpq(6)
+//   .lpenv(slider(2.6, 0.8, 5))
+//   .gain(0.38)
+
+let bassline = stack(bass_sub, bass_mid)
 
 let count_in = s("[hh]*4").bank("RolandTR909").lpf(1800).lpenv(0.5).decay(3).room(0.01).gain(0.3)
 
 offset = 6 - 1/2
 $: arrange(
-  [offset,     stack(kick, bassline)],
-  [1/2,        stack(kick, bassline.late(offset), count_in.slow(1/2))],
-  [4294967296, stack(kick, bassline)],
+  [offset,     stack(kick, hihat, bassline)],
+  [1/2,        stack(kick, hihat, bassline.late(offset), count_in.slow(1/2))],
+  [4294967296, stack(kick, hihat, bassline)],
 )
