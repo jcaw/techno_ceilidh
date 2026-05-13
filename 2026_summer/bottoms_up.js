232ac1d76021d9150718716763cb2631d5ba0b69
jcaw <toastedjcaw@gmail.com>
2026-05-13 17:40:42 +0100

    Revert glitched version

--- 2026_summer/bottoms_up.js
+++ 2026_summer/bottoms_up.js
@@ -1,4 +1,4 @@
-// Bottoms Up
+// Bonus - Chloe's Passion / Farewell to Whalley Range
 let part = 1
 
 // UNCOMMENT PART 2 THEN CLICK "update" TO SWITCH TO PART 2
@@ -8,59 +8,92 @@ let part = 1
 /////////////////////////////////////////////////////
 // CODE BELOW - DO NOT TOUCH
 
-setCpm(120/4)
+setCpm(72/8)
 
-let kick = s("[bd ~ bd ~ bd ~ bd ~]*4").bank("RolandTR909")
-  .lpf(260)
-  .lpenv(2.4)
-  .decay(0.68)
+let kick = s("[bd ~ ~]*3*4").bank("RolandTR909")
+  .lpf(sine.range(190, 980).slow(6))
+  .lpenv(2.6)
+  .decay(0.72)
   .room(0.12)
-  .clip(1.16)
-  .distort(0.15)
+  .clip(1.14)
+  .distort(0.16)
   .gain(0.92)
+
+let up     = n("[0 1 2]*3")
+let down   = n("[2 1 0]*3")
+let mixed  = n("[2 0 1]*3")
+let mixed2 = n("[0 2 1]*3")
 
 let full_notes
 if (part == 1) {
-  // The Humours of Ennistymon
+  // TODO: Switch to the chords from Humours of Ennistymon
+  // Chloe's Passion
+  let Em = up.chord("Em").voicing()
+  let D_ = up.chord("D").voicing()
+  let G_ = up.chord("G").voicing()
+  let C_ = mixed.chord("C").voicing()
+  let Bm = up.chord("Bm").voicing()
+
   full_notes = cat(
-    "[g1 a1] d1 [g1 a1] d1",
-    "[g1 a1] d1 d1 g1",
-    "d1 d1 a1 d1",
-    "d1 d1 d1 [d1 g1]",
-    "[g1 a1] g1 [a1 d1] d1",
-    "[g1 a1] e1 a1 [d1 g1]",
-  ).note()
+    [Em, D_, Em, D_],
+    [Em, Em, Em, D_],
+    [G_, [G_, C_], [G_, C_], D_],
+    [Em, D_, Em, [D_, Bm]],
+    [Em, D_, G_, D_],
+  ).transpose(-24)
 } else {
-  // The Lark in the Morning
+  // TODO: Switch to the chords from The Lark in the Morning
+  // Farewell to Whalley Range
+  let Fsm = up.chord("F#m").voicing()
+  let A   = down.chord("A").voicing()
+  let Bm  = up.chord("Bm").voicing()
+  let Csm = up.chord("C#m").voicing()
+  let D   = mixed2.chord("D").voicing()
+  let E   = up.chord("E").voicing()
+
   full_notes = cat(
-    "d1 g1 d1 [d1 g1]",
-    "d1 g1 d1 [a1 b1 g1]",
-    "d1 [g1 a1] d1 [a1 b1 g1]",
-    "d1 [g1 d1] [g1 d1] [e1 g1]",
-    "d1 d1 d1 [e1 g1]",
-    "d1 d1 [g1 d1] [e1 g1]",
-    "d1 d1 d1 [e1 g1]",
-    "d1 [a1 d1] [g1 d1] [e1 g1]",
-  ).note()
+    [Fsm, A, Bm, Csm],
+    [Fsm, A, Bm, Csm],
+    [D,   E, Bm, Csm],
+    [D,   E, Bm, Csm],
+  ).transpose(-24)
 }
 
-let bassline = full_notes.s("square").decay(0.22)
-  .struct("[~ x ~ x ~ x ~ x]*4").slow(1)
+
+let bass_laser = s("supersaw").decay(0.2)
+  .struct("[x x x]*3*4").slow(1)
+  //.clip(sine.range(0.8, 1.0).slow(8))
   .sustain(0)
-  .clip(1.25)
-  .distort(0.2)
-  .lpf(slider(360, 120, 2200))
-  .lpq(7)
-  .lpenv(slider(4.8, 0.8, 7))
-  .delay(0.14)
-  .gain(0.38)
+  .clip(0.9)
+  .distort(0.18)
+  .lpf(sine.range(200, 1200).slow(3))
+  .lpq(6)
+  .lpenv(sine.range(1.2, 6).slow(4))
+  .delay(0.12)
+  .gain(0.8)
 
 
-let count_in = s("[hh]*4").bank("RolandTR909").lpf(1800).lpenv(0.5).decay(3).room(0.01).gain(0.3)
+let bassline = set(full_notes, bass_laser)
 
-offset = 2 - 1/4
+
+let chops = note("e3 e3 e3").s("sawtooth").vowel("a i o").vibmod("0.1:2")
+  .fm(1)
+  .fmattack(".1")
+  .lpf(slider(816, 0, 2000))
+  .room(0.1)
+  .transpose([0, -12])
+  .clip(0.5).gain(2)
+
+
+let count_length = 1/4
+let kick_offset  = 2 - count_length
 $: arrange(
-  [offset,     stack(kick, bassline)],
-  [1/4,        stack(kick, bassline.late(offset), count_in.slow(1/4))],
-  [4294967296, stack(kick, bassline)],
+  // Just arpeggios first
+  [2,           bassline],
+  // Bring in the kick
+  [kick_offset, stack(kick, bassline.late(2))],
+  // Count in
+  [count_length,stack(kick, bassline.late(kick_offset), chops.slow(1/4))],
+  // And go
+  [4294967296,  stack(kick, bassline)],
 )
